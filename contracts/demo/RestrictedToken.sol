bcfa552f845c36e99444fc1f11f725526381fea4
Cyril Lapinte <cyril.lapinte@free.fr>
2018-09-11 18:08:02 +0200

    Adding @notice to legal notice

--- contracts/demo/RestrictedToken.sol
+++ contracts/demo/RestrictedToken.sol
@@ -15,12 +15,12 @@ import "./KnowYourCustomer.sol";
  *
  * @author Cyril Lapinte - <cyril.lapinte@mtpelerin.com>
  *
- * Copyright © 2016 - 2018 Mt Pelerin Group SA - All Rights Reserved
- * This content cannot be used, copied or reproduced in part or in whole
- * without the express and written permission of Mt Pelerin Group SA.
- * Written by *Mt Pelerin Group SA*, <info@mtpelerin.com>
- * All matters regarding the intellectual property of this code or software
- * are subjects to Swiss Law without reference to its conflicts of law rules.
+ * @notice Copyright © 2016 - 2018 Mt Pelerin Group SA - All Rights Reserved
+ * @notice This content cannot be used, copied or reproduced in part or in whole
+ * @notice without the express and written permission of Mt Pelerin Group SA.
+ * @notice Written by *Mt Pelerin Group SA*, <info@mtpelerin.com>
+ * @notice All matters regarding the intellectual property of this code or software
+ * @notice are subjects to Swiss Law without reference to its conflicts of law rules.
  */
 contract RestrictedToken is StandardToken, KnowYourCustomer, Pausable {
 
