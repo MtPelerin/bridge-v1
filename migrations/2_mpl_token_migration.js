6d3b6e23b3629b85412c47680d6377f00989188d
Cyril Lapinte <cyril.lapinte@free.fr>
2018-10-15 21:09:39 +0200

    Updating token migration

--- migrations/2_mpl_token_migration.js
+++ migrations/2_mpl_token_migration.js
@@ -1,24 +1,24 @@
 var UserRegistry = artifacts.require('./UserRegistry.sol');
 var MPSSaleConfig = artifacts.require('./mps/MPSSaleConfig.sol');
-var MPSBridgeToken = artifacts.require('../mps/MPSBridgeToken.sol');
+var MPSToken = artifacts.require('../mps/MPSToken.sol');
 var TokenMinter = artifacts.require('./tokensale/TokenMinter.sol');
 
 module.exports = function (deployer, network, accounts) {
   return deployer.deploy([
     [ UserRegistry, [], 0 ],
     [ MPSSaleConfig ],
-    [ MPSBridgeToken ],
+    [ MPSToken ],
   ]).then(function () {
     return deployer.deploy([
       [ TokenMinter, MPSSaleConfig.address, accounts[0], [ accounts[1], accounts[2] ] ],
     ]);
   }).then(function () {
-    return MPSBridgeToken.at(
-      MPSBridgeToken.address
+    return MPSToken.at(
+      MPSToken.address
     ).transferOwnership(TokenMinter.address);
   }).then(function () {
     return TokenMinter.at(
       TokenMinter.address
-    ).setup(MPSBridgeToken.address, [ accounts[3], accounts[4] ]);
+    ).setup(MPSToken.address, [ accounts[3], accounts[4] ]);
   });
 };
