2d5bcd2f2b3ed90957a4428ceda4aa23cbe0c476
Cyril Lapinte <cyril.lapinte@free.fr>
2018-10-27 19:01:05 +0200

    Adding Operator

---
+++ contracts/Operator.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.4.23;
+
+import "./zeppelin/ownership/Ownable.sol";
+
+
+/**
+ * @title Operator
+ * @dev The Operator contract contains list of addresses authorized to specific operations
+ *
+ * @author Cyril Lapinte - <cyril.lapinte@mtpelerin.com>
+ *
+ * @notice Copyright © 2016 - 2018 Mt Pelerin Group SA - All Rights Reserved
+ * @notice This content cannot be used, copied or reproduced in part or in whole
+ * @notice without the express and written permission of Mt Pelerin Group SA.
+ * @notice Written by *Mt Pelerin Group SA*, <info@mtpelerin.com>
+ * @notice All matters regarding the intellectual property of this code or software
+ * @notice are subjects to Swiss Law without reference to its conflicts of law rules.
+ *
+ * Error messages
+ * OP01: Message sender must be an authority
+ */
+contract Operator is Ownable {
+
+  mapping (address => uint8) operatorIds;
+  address[] public operators;
+
+  /**
+   * @dev Throws if called by any account other than the operator
+   */
+  modifier onlyOperator {
+    require(operatorIds[msg.sender] > 0, "OP01");
+    _;
+  }
+
+  /**
+   * @dev return the count of operator
+   */
+  function operatorCount() public view returns (uint256) {
+    return operators.length;
+  }
+
+  /**
+   * @dev return the address associated to the _operatorId
+   */
+  function operatorAddress(uint8 _operatorId) public view returns (address) {
+    return operators[_operatorId];
+  }
+
+  /**
+   * @dev defineOperator role
+   * @param _names operators names
+   * @param _addresses operator addresses.
+   */
+  function defineOperators(bytes32[] _names, address[] _addresses) public onlyOwner {
+    require(_names.length == _addresses.length, "OP02");
+
+    for(uint256 i = 0; i < operators.length; i++) {
+      delete operatorIds[operators[i]];
+    }
+    emit OperatorsCleared(operators.length);
+
+    for(uint256 j = 0; j < _names.length; j++) {
+      emit OperatorDefined(_names[j], _addresses[j]);
+      operatorIds[_addresses[j]] = uint8(j + 1);
+    }
+    operators = _addresses;
+  }
+
+  event OperatorsCleared(uint256 size);
+  event OperatorDefined(
+    bytes32 name,
+    address _address
+  );
+}
---
+++ test/Operator.js
@@ -0,0 +1,106 @@
+'user strict';
+
+/**
+ * @author Cyril Lapinte - <cyril.lapinte@mtpelerin.com>
+ *
+ * Copyright © 2016 - 2018 Mt Pelerin Group SA - All Rights Reserved
+ * This content cannot be used, copied or reproduced in part or in whole
+ * without the express and written permission of Mt Pelerin Group SA.
+ * Written by *Mt Pelerin Group SA*, <info@mtpelerin.com>
+ * All matters regarding the intellectual property of this code or software
+ * are subjects to Swiss Law without reference to its conflicts of law rules.
+ *
+ */
+
+const assertRevert = require('./helpers/assertRevert');
+var OperatorMock = artifacts.require('../contracts/mock/OperatorMock.sol');
+
+contract('Operator', function (accounts) {
+  let operator;
+
+  beforeEach(async function () {
+    operator = await OperatorMock.new();
+  });
+
+  it('should have no operator', async function () {
+    const operatorCount = await operator.operatorCount();
+    assert.equal(operatorCount, 0, 'count');
+  });
+
+  it('should allow owner to set a new operator', async function () {
+    const tx = await operator.defineOperators([ 'OPERATOR' ], [ accounts[2] ]);
+    assert.equal(parseInt(tx.receipt.status), 1, 'status');
+    assert.equal(tx.logs.length, 2);
+    assert.equal(tx.logs[0].event, 'OperatorsCleared');
+    assert.equal(tx.logs[0].args.size, 0);
+    assert.equal(tx.logs[1].event, 'OperatorDefined');
+    assert.equal(web3.toAscii(tx.logs[1].args.name).replace(/\x00+$/, ''), 'OPERATOR');
+    assert.equal(tx.logs[1].args._address, accounts[2]);
+  });
+
+  it('should allow owner to set new operators', async function () {
+    const tx = await operator.defineOperators([ 'OPERATOR1', 'OPERATOR2' ], [ accounts[2], accounts[3] ]);
+    assert.equal(parseInt(tx.receipt.status), 1, 'status');
+    assert.equal(tx.logs.length, 3);
+    assert.equal(tx.logs[0].event, 'OperatorsCleared');
+    assert.equal(tx.logs[0].args.size, 0);
+    assert.equal(tx.logs[1].event, 'OperatorDefined');
+    assert.equal(web3.toAscii(tx.logs[1].args.name).replace(/\x00+$/, ''), 'OPERATOR1');
+    assert.equal(tx.logs[1].args._address, accounts[2]);
+    assert.equal(tx.logs[2].event, 'OperatorDefined');
+    assert.equal(web3.toAscii(tx.logs[2].args.name).replace(/\x00+$/, ''), 'OPERATOR2');
+    assert.equal(tx.logs[2].args._address, accounts[3]);
+  });
+
+  it('should not allow owner to set new operators with wrong operator/name count', async function () {
+    await assertRevert(operator.defineOperators([ 'OPERATOR', 'OPERATOR2' ], [ accounts[2] ]));
+  });
+
+  it('should not allow non owner to set a new operator', async function () {
+    await assertRevert(operator.defineOperators([ 'OPERATOR' ], [ accounts[2] ], { from: accounts[4] }));
+  });
+
+  describe('with authorities defined', function () {
+    beforeEach(async function () {
+      await operator.defineOperators([ 'LEGAL', 'REGULATOR' ], [ accounts[1], accounts[2] ]);
+    });
+
+    it('should have two operators', async function () {
+      const count = await operator.operatorCount();
+      assert.equal(count, 2, 'count');
+    });
+
+    it('should return accounts1 for operator1', async function () {
+      const op1 = await operator.operatorAddress(0);
+      assert.equal(op1, accounts[1], 'account 1');
+    });
+
+    it('should return accounts1 for operator2', async function () {
+      const op2 = await operator.operatorAddress(1);
+      assert.equal(op2, accounts[2], 'account 2');
+    });
+
+    it('should allow operator1 through onlyOperator modifier', async function () {
+      await operator.testOnlyOperator({ from: accounts[1] });
+    });
+
+    it('should allow operator2 through onlyOperator modifier', async function () {
+      await operator.testOnlyOperator({ from: accounts[2] });
+    });
+
+    it('should not allow non operator through onlyOperator modifier', async function () {
+      await assertRevert(operator.testOnlyOperator());
+    });
+
+    it('should allow owner to set a new operator', async function () {
+      const tx = await operator.defineOperators([ 'OPERATOR' ], [ accounts[2] ]);
+      assert.equal(parseInt(tx.receipt.status), 1, 'status');
+      assert.equal(tx.logs.length, 2);
+      assert.equal(tx.logs[0].event, 'OperatorsCleared');
+      assert.equal(tx.logs[0].args.size, 2);
+      assert.equal(tx.logs[1].event, 'OperatorDefined');
+      assert.equal(web3.toAscii(tx.logs[1].args.name).replace(/\x00+$/, ''), 'OPERATOR');
+      assert.equal(tx.logs[1].args._address, accounts[2]);
+    });
+  });
+});
